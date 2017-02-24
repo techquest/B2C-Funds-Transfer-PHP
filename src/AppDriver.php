43471bf5c03512ed7493dba4957b58ab3dc9fe24
Ugochukwu Okeke <ugochukwu.okeke@Interswitchng.com>
2017-02-24 16:27:50 +0100

    IA-6058

--- src/AppDriver.php
+++ src/AppDriver.php
@@ -1,6 +1,22 @@
 <?php
+    use Interswitch\Interswitch;
     use Interswitch\transfer\fundtransfer;
     require_once __DIR__ . '/../vendor/autoload.php';
-    $cl = new FundTransfer("","","");
+
+    $initiatingEntityCode = "PBL";
+    $clientId = "IKIA2EFBE1EF63D1BBE2AF6E59100B98E1D3043F477A";
+    $clientSecret = "uAk0Amg6NQwQPcnb9BTJzxvMS6Vz22octQglQ1rfrMA=";
+
+    
+        /***- START- ***/
+
+        /**
+         * Interswitch.ENV_SANDBOX, is for sandbox environment
+         * 
+         * Interswitch.ENV_PROD, is for production environment
+         */
+    $transfer = new FundTransfer($clientId,$clientSecret,Interswitch::ENV_DEV);
+
+    $bankResponse = $transfer->fetchBanks();
     
 ?>
--- src/transfer/fundtransfer.php
+++ src/transfer/fundtransfer.php
@@ -1,17 +1,46 @@
 <?php 
     namespace Interswitch\transfer;
+    use Interswitch\Interswitch;
+    use Interswitch\transfer;
+    require_once __DIR__ . '/../../vendor/autoload.php';
 
     class FundTransfer {
 
         private $clientId;
         private $clientSecret;
         private $environment;
+        private $interswitch;
+        const ATM = "1";
+        const WEB = "3";
+        const MOBILE = "4";
+        const KIOSK = "5";
+        const PCPOS = "6";
+        const POS = "2";
+        const LOCATION = "7";
+        const DIRECT_DEBIT = "8";
 
-        public function __construct($clientId="", $clientSecret="", $env="SANDBOX"){
-            echo "creating class ";
+        public function __construct($clientId, $clientSecret, $env="SANDBOX"){
+            
             $this->clientId = $clientId;
             $this->clientSecret = $clientSecret;
             $this->environment = $env;
+            $this->interswitch = new Interswitch($clientId, $clientSecret, Interswitch::ENV_DEV);
         }
+
+        public function send($transfer) {
+
+
+        }
+
+        public function fetchBanks() {
+
+            $response = $this->interswitch->send(Constants::GET_ALL_BANKS_RESOURCE_URL, Constants::GET);
+
+        }
+
+        public function validateAccount($transfer){
+
+
+        } 
     }
 ?>
